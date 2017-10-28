152c227595b65bcd86c8f3485db2d42cd95d4217
NeuronCrossroads <ary.dhatt@gmail.com>
2017-10-27 21:56:20 -0500

    Changes to input program for pixel intensities
    Changed max intensity from 200 to 254 instead of 255

--- Interface.js
+++ Interface.js
@@ -50,34 +50,34 @@ function draw() {
 function fillCircle(gridPosX, gridPosY) {
   noStroke();
   //Center
-  data.setValue(gridPosX,gridPosY,maxm(data.getValue(gridPosX,gridPosY)+125,200));
+  data.setValue(gridPosX,gridPosY,maxm(data.getValue(gridPosX,gridPosY)+125,254));
   fill(0,0,0,255/2);
   rect(gridPosX*(gridInterval),gridPosY*(gridInterval), gridInterval, gridInterval);
   //Right
   if ( gridPosX+1 < 28 ) {
     //1 right
-    data.setValue(gridPosX+1,gridPosY,maxm(data.getValue(gridPosX+1,gridPosY)+85,200));
+    data.setValue(gridPosX+1,gridPosY,maxm(data.getValue(gridPosX+1,gridPosY)+85,254));
     fill(0,0,0,255/3);
     rect((gridPosX+1)*(gridInterval),gridPosY*(gridInterval), gridInterval, gridInterval);
   }
   //Left
   if ( gridPosX-1 >= 0 ) {
     //1 left
-    data.setValue(gridPosX-1,gridPosY,maxm(data.getValue(gridPosX-1,gridPosY)+85,200));
+    data.setValue(gridPosX-1,gridPosY,maxm(data.getValue(gridPosX-1,gridPosY)+85,254));
     fill(0,0,0,255/3);
     rect((gridPosX-1)*(gridInterval),gridPosY*(gridInterval), gridInterval, gridInterval);
   }
   //Up
   if ( gridPosY-1 >= 0 ) {
     //1 left
-    data.setValue(gridPosX,gridPosY-1,maxm(data.getValue(gridPosX,gridPosY-1)+85,200));
+    data.setValue(gridPosX,gridPosY-1,maxm(data.getValue(gridPosX,gridPosY-1)+85,254));
     fill(0,0,0,255/3);
     rect(gridPosX*(gridInterval),(gridPosY-1)*(gridInterval), gridInterval, gridInterval);
   }
   //Down
   if ( gridPosY+1 < 28 ) {
     //1 left
-    data.setValue(gridPosX,gridPosY+1,maxm(data.getValue(gridPosX,gridPosY+1)+85,200));
+    data.setValue(gridPosX,gridPosY+1,maxm(data.getValue(gridPosX,gridPosY+1)+85,254));
     fill(0,0,0,255/3);
     rect(gridPosX*(gridInterval),(gridPosY+1)*(gridInterval), gridInterval, gridInterval);
   }
