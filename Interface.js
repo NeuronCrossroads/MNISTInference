1d55a713b89c70a704657ce449d63d493f55bf70
NeuronCrossroads <ary.dhatt@gmail.com>
2017-10-28 13:24:23 -0500

    Edited input pixel intensities

--- Interface.js
+++ Interface.js
@@ -50,35 +50,35 @@ function draw() {
 function fillCircle(gridPosX, gridPosY) {
   noStroke();
   //Center
-  data.setValue(gridPosX,gridPosY,maxm(data.getValue(gridPosX,gridPosY)+125,254));
-  fill(0,0,0,255/2);
+  data.setValue(gridPosX,gridPosY,maxm(data.getValue(gridPosX,gridPosY)+254,254));
+  fill(0,0,0,254);
   rect(gridPosX*(gridInterval),gridPosY*(gridInterval), gridInterval, gridInterval);
   //Right
   if ( gridPosX+1 < 28 ) {
     //1 right
-    data.setValue(gridPosX+1,gridPosY,maxm(data.getValue(gridPosX+1,gridPosY)+85,254));
-    fill(0,0,0,255/3);
+    data.setValue(gridPosX+1,gridPosY,maxm(data.getValue(gridPosX+1,gridPosY)+185,254));
+    fill(0,0,0,185);
     rect((gridPosX+1)*(gridInterval),gridPosY*(gridInterval), gridInterval, gridInterval);
   }
   //Left
   if ( gridPosX-1 >= 0 ) {
     //1 left
-    data.setValue(gridPosX-1,gridPosY,maxm(data.getValue(gridPosX-1,gridPosY)+85,254));
-    fill(0,0,0,255/3);
+    data.setValue(gridPosX-1,gridPosY,maxm(data.getValue(gridPosX-1,gridPosY)+185,254));
+    fill(0,0,0,185);
     rect((gridPosX-1)*(gridInterval),gridPosY*(gridInterval), gridInterval, gridInterval);
   }
   //Up
   if ( gridPosY-1 >= 0 ) {
     //1 left
-    data.setValue(gridPosX,gridPosY-1,maxm(data.getValue(gridPosX,gridPosY-1)+85,254));
-    fill(0,0,0,255/3);
+    data.setValue(gridPosX,gridPosY-1,maxm(data.getValue(gridPosX,gridPosY-1)+185,254));
+    fill(0,0,0,185);
     rect(gridPosX*(gridInterval),(gridPosY-1)*(gridInterval), gridInterval, gridInterval);
   }
   //Down
   if ( gridPosY+1 < 28 ) {
     //1 left
-    data.setValue(gridPosX,gridPosY+1,maxm(data.getValue(gridPosX,gridPosY+1)+85,254));
-    fill(0,0,0,255/3);
+    data.setValue(gridPosX,gridPosY+1,maxm(data.getValue(gridPosX,gridPosY+1)+185,254));
+    fill(0,0,0,185);
     rect(gridPosX*(gridInterval),(gridPosY+1)*(gridInterval), gridInterval, gridInterval);
   }
 }
